4085fe97ad4c2c1fe9beb12952ca9c21c673e74b
ChamudiM <chamudimalsha645@gmail.com>
2024-06-24 18:20:32 +0530

    upto OAuth Component

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -15,6 +15,10 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
+    profilePicture:{
+        type: String,
+        default: "https://thumbs.dreamstime.com/b/default-placeholder-profile-icon-avatar-gray-man-90197957.jpg",
+    }
 },{timeseries: true}); //the time of creation and the time of edit
 
 userSchema.index({ username: 1 }, { unique: true });
