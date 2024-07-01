cac7deeb1f7ad7e53ed260dd411f37e412cbbb3a
ChamudiM <chamudimalsha645@gmail.com>
2024-07-02 02:38:17 +0530

    complete profile page ui

--- frontend/src/pages/UserProfile.jsx
+++ frontend/src/pages/UserProfile.jsx
@@ -1,7 +1,106 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const UserProfile = () => {
-  return <div>UserProfile</div>;
+  const currentUser = useSelector((state) => state.user.currentUser);
+  return (
+    <>
+      <div className="flex justify-center items-center h-screen">
+        <form className="w-full max-w-3xl px-20">
+          <div>
+            <h1 className="pb-5 text-2xl font-semibold leading-7 text-gray-900">Profile</h1>
+            <div className="col-span-full">
+              <div className="mt-2 flex items-center gap-x-2">
+                <img
+                  src={currentUser.profilePicture}
+                  alt="profile"
+                  className="h-24 w-24 self-center rounded-full object-cover"
+                />
+                <button
+                  type="button"
+                  className="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+                >
+                  Change
+                </button>
+              </div>
+            </div>
+
+            <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+              <div className="sm:col-span-4">
+                <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">
+                  Username
+                </label>
+                <div className="mt-2">
+                  <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-yellow-600 sm:max-w-md">
+                    <input
+                    defaultValue={currentUser.username}
+                      type="text"
+                      name="username"
+                      id="username"
+                      autoComplete="username"
+                      className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                      placeholder="username"
+                    />
+                  </div>
+                </div>
+              </div>
+
+              <div className="sm:col-span-4">
+                <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                  Email address
+                </label>
+                <div className="mt-2">
+                  <input
+                  defaultValue={currentUser.email}
+                    id="email"
+                    name="email"
+                    type="email"
+                    autoComplete="email"
+                    placeholder="Email"
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-yellow-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+
+              <div className="sm:col-span-4">
+                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                  Password
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="password"
+                    name="password"
+                    type="password"
+                    placeholder="Password"
+                    autoComplete="current-password"
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-yellow-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+
+            </div>
+          </div>
+
+          <div className="mt-6 flex items-center justify-start gap-x-4">
+          
+            <button
+              type="submit"
+              className="rounded-md bg-yellow-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-yellow-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-yellow-600"
+            >
+              Update Profile
+            </button>
+            <button
+              type="submit"
+              className="rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
+            >
+              Delete Account
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
 };
 
 export default UserProfile;
