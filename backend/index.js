fdc6b2086aefb2a8e54fde1a74500529ae152f5a
Chamudi Malsha <161874193+ChamudiM@users.noreply.github.com>
2024-08-06 18:15:01 +0530

    origin updated

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json()); //alow json as the input of our backend
 app.use(cookieParser());
 //Middleware for handling CORS POLICY
 app.use(cors({
-    origin: [process.env.FRONTEND_URL],
+    origin: [process.env.FRONTEND_URL,'https://appete.onrender.com'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type'],
     credentials: true, // Allow credentials (cookies)
