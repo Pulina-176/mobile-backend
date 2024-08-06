1d4151e8d532226bc4e22b958bca1848f97007a3
Chamudi Malsha <161874193+ChamudiM@users.noreply.github.com>
2024-08-06 15:37:02 +0530

    backend redeploy

--- backend/index.js
+++ backend/index.js
@@ -16,9 +16,10 @@ app.use(express.json()); //alow json as the input of our backend
 app.use(cookieParser());
 //Middleware for handling CORS POLICY
 app.use(cors({
-    origin: '*',
+    origin: ['http://localhost:5173', 'https://appete.onrender.com', 'https://appete-service.onrender.com/'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type'],// Allow credentials (cookies)
+    allowedHeaders: ['Content-Type'],
+    credentials: true, // Allow credentials (cookies)
   }));//to recieve data from different URLS
 app.use(helmet());
 
